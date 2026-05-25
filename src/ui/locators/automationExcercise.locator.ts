cba646f849fe6954c9004d2fb4e943dcc7a2d2f4
Saravanan Sundar <2000185758@hexaware.com>
2026-05-25 14:13:14 +0530

    Pushing latest Changes

--- src/ui/locators/automationExcercise.locator.ts
+++ src/ui/locators/automationExcercise.locator.ts
@@ -2,5 +2,6 @@ export class AutomationExcerciseLocators {
   static readonly emailAddress = 'input[data-qa="login-email"]';
   static readonly password= 'input[data-qa="login-password"]';
   static readonly loginButton = 'button[data-qa="login-button"]';
+  //static readonly loginButton = (value: string) =>`button[data-qa="${value}"]`;
   static readonly productsLabel = 'a[href="/products"]:has-text("Products")';
 }
